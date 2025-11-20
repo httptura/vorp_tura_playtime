f0134bb5af30d5f9621db91dfaf459a44db24e82
httptura <fpslaze@gmail.com>
2025-11-20 21:57:05 +0100

    Remove rewards configuration from config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,1 @@
 Config = {}
-
-Config.Rewards = {
-    [10] = {
-        items = {bread = 10, water = 10},
-        money = 50.00
-    },
-    [20] = {
-        items = {bread = 10, water = 10},
-        money = 50.00
-    },
-}
